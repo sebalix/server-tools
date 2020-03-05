3e5cd44727f99a5cbaf5726f7f28afbc0a3af919
sebalix <seb@usr-src.org>
2020-03-05 17:35:57 +0100

    [FIX] binary_field: the log done in JS makes unusable some views if the field has no value defined

--- binary_field/static/src/js/widget.js
+++ binary_field/static/src/js/widget.js
@@ -50,7 +50,6 @@ openerp.binary_field = function (instance) {
 
     instance.web_kanban.KanbanRecord.include({
         kanban_image: function(model, field, id, cache, options) {
-            console.log(this.record[field].value)
             if (this.record[field]
                 && this.record[field].value
                 && instance.web.form.is_url(this.record[field].value)
